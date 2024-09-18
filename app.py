2c60f4e6b0cbea300d7a8ea341c19b988c116e3f
Daniboss101 <136389310+Daniboss101@users.noreply.github.com>
2024-09-18 13:30:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import warnings
+
+# Suppress specific warnings related to numpy binary compatibility
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
 import logging
 from logging.handlers import RotatingFileHandler
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5000"}})
@@ -18,11 +23,11 @@ from forecast_module import forecast_blueprint
 # Register weather blueprint
 app.register_blueprint(weather_blueprint)
 app.register_blueprint(forecast_blueprint)
+
 @app.route('/')
 def index():
     app.logger.info('Processing default request')
-    return "hello world"
-
+    return redirect('/weather')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
